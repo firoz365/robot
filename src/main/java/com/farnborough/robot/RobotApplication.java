c37a0b7c52d3f50df9f8ce6ffa3f450f5ad27e4e
firoz <firoz365@gmail.com>
2020-02-04 23:24:15 +0000

    Adding reguler expression and reading from system.in

--- src/main/java/com/farnborough/robot/RobotApplication.java
+++ src/main/java/com/farnborough/robot/RobotApplication.java
@@ -3,11 +3,40 @@ package com.farnborough.robot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class RobotApplication {
 
+	static String PLACE_PATTERN="(?i)PLACE\\s+[0-4]\\s*,\\s*[0-4]\\s*,\\s*[ NORTH | SOUTH |EAST|WEST]\\s*";
+	static String MOVE_PATTERN="(?i)\\s*MOVE";
+	static String LEFT_PATTERN="(?i)\\s*LEFT";
+	static String RIGHT_PATTERN="(?i)\\s*RIGHT";
+	static String REPORT_PATTERN="(?i)\\s*REPORT";
+
 	public static void main(String[] args) {
-		SpringApplication.run(RobotApplication.class, args);
+		//SpringApplication.run(RobotApplication.class, args);
+
+		boolean isRobotPlaced = false ;
+		Scanner scanner = new Scanner(System.in);
+
+		boolean exit = false;
+		while(!exit){
+			String text = scanner.nextLine();
+			System.out.println(text);
+
+			if(!isRobotPlaced){
+				if(!text.matches(PLACE_PATTERN))
+					throw new RuntimeException("First command must be place");
+				else
+					isRobotPlaced = true;
+
+			}
+
+
+			if(text.equalsIgnoreCase("exit") || text.equalsIgnoreCase("!q"))
+				exit = true;
+		}
 	}
 
 }
